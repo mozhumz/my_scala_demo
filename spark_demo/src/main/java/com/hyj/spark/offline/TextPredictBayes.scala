06d32dd6653deed9902d7f02e05dc8c4e67dbbe2
mozhumz <554501252@qq.com>
2020-04-04 16:49:58 +0800

    修改配置-windows环境运行linux的hdfs

---
+++ spark_demo/src/main/java/com/hyj/spark/offline/TextPredictBayes.scala
@@ -0,0 +1,20 @@
+package com.hyj.spark.offline
+
+import org.apache.spark.sql.SparkSession
+
+object TextPredictBayes {
+  def main(args: Array[String]): Unit = {
+    System.setProperty("HADOOP_USER_NAME","root")
+//    val warehouse = "hdfs://192.168.163.130:9000/user/hive/warehouse"
+
+    val spark = SparkSession.builder()
+      .master("local[2]")
+      .appName("User Base")
+      .enableHiveSupport()
+      .getOrCreate()
+
+    println(spark)
+
+    spark.sql("select * from badou.orders").show()
+  }
+}
