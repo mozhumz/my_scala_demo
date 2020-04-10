019968502f7aa2ea4e31b80382936f9495891a32
mozhumz <554501252@qq.com>
2020-04-10 10:23:38 +0800

    自定义flume拦截器

--- flume_demo/src/main/java/com/hyj/flume/interceptor/MyInterceptor.java
+++ flume_demo/src/main/java/com/hyj/flume/interceptor/MyInterceptor.java
@@ -5,31 +5,37 @@ import org.apache.flume.Context;
 import org.apache.flume.Event;
 import org.apache.flume.interceptor.Interceptor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MyInterceptor implements Interceptor {
 
+    private List<Event> eventList;
 
     @Override
     public void initialize() {
-
+        eventList = new ArrayList<>();
     }
 
     @Override
     public Event intercept(Event event) {
-        String body=new String(event.getBody());
-        if(body.contains("hello")){
-            event.getHeaders().put("hyj","hyj");
-        }else {
-            event.getHeaders().put("hyj","gcd");
+        String body = new String(event.getBody());
+        if (body.contains("hello")) {
+            event.getHeaders().put("hyj", "mozhu");
+        } else {
+            event.getHeaders().put("hyj", "gcd");
         }
         return event;
     }
 
     @Override
     public List<Event> intercept(List<Event> list) {
-        if(CollectionUtils.isNotEmpty(list)){
-            list.forEach(this::intercept);
+//        eventList.clear();
+        if (CollectionUtils.isNotEmpty(list)) {
+            list.forEach(event -> {
+                intercept(event);
+//                eventList.add(event);
+            });
         }
         return list;
     }
@@ -39,11 +45,11 @@ public class MyInterceptor implements Interceptor {
 
     }
 
-    public static class BuilderHyj implements Interceptor.Builder{
+    public static class BuilderHyj implements Interceptor.Builder {
 
         @Override
         public Interceptor build() {
-            return null;
+            return new MyInterceptor();
         }
 
         @Override
