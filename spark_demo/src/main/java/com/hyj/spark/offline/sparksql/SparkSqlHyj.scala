13e9a4d511524d2ff1bf406fad2221f2aeb7a5a7
mozhumz <554501252@qq.com>
2020-06-06 22:35:33 +0800

    sparksql写入mysql

--- spark_demo/src/main/java/com/hyj/spark/offline/sparksql/SparkSqlHyj.scala
+++ spark_demo/src/main/java/com/hyj/spark/offline/sparksql/SparkSqlHyj.scala
@@ -1,5 +1,8 @@
 package com.hyj.spark.offline.sparksql
 
+import java.util.Properties
+
+import com.hyj.spark.offline.sparksql.config.MySqlConf
 import org.apache.spark.sql.SparkSession
 
 object SparkSqlHyj {
@@ -11,15 +14,38 @@ object SparkSqlHyj {
       .appName("Feature Transform")
 //      .config("hive.metastore.uris","thrift://master:9083")
       //本地调试要加上
-//      .master("local[*]")
+      .master("local[*]")
       .enableHiveSupport()
       .getOrCreate()
     //    spark shell [client] 实例化sc,spark
-    val orders = spark.sql("select * from badou.orders limit 10")
-    val priors = spark.sql("select * from badou.priors limit 10")
+    val orders = spark.sql("select * from badou.orders where order_id!='order_id' ")
+    val priors = spark.sql("select * from badou.priors where order_id!='order_id' ")
 
     orders.show()
     println("------------------------------------")
     priors.show()
+
+    //将数据写入mysql-badou
+    val properties: Properties = new Properties
+    properties.put("user",MySqlConf.username)
+    properties.put("password",MySqlConf.password)
+    properties.put("batchsize","10000")
+    println("------write orders start--------------")
+    /**
+      * 默认为SaveMode.ErrorIfExists模式，该模式下，如果数据库中已经存在该表，则会直接报异常，导致数据不能存入数据库.另外三种模式如下：
+      * SaveMode.Append 如果表已经存在，则追加在该表中；若该表不存在，则会先创建表，再插入数据；
+      * SaveMode.Overwrite 重写模式，其实质是先将已有的表及其数据全都删除，再重新创建该表，最后插入新的数据；
+      * SaveMode.Ignore 若表不存在，则创建表，并存入数据；在表存在的情况下，直接跳过数据的存储，不会报错。
+      */
+    //    orders.write
+    //      .mode(SaveMode.Append)
+    //      .jdbc()
+    orders.write.jdbc(MySqlConf.url,"orders",properties)
+    println("------write orders ok--------------")
+    println("------write priors start--------------")
+    priors.write.jdbc(MySqlConf.url,"priors",properties)
+    println("------write priors ok--------------")
   }
+
+
 }
---
+++ spark_demo/src/main/java/com/hyj/spark/offline/sparksql/config/MySqlConf.scala
@@ -0,0 +1,17 @@
+package com.hyj.spark.offline.sparksql.config
+
+import java.sql.{Connection, DriverManager, Statement}
+
+object MySqlConf {
+  val url="jdbc:mysql://master:3306/badou?useUnicode=true&characterEncoding=utf-8&useSSL=false&rewriteBatchedStatements=true"
+  val username="root"
+  val password="123456"
+  val driver="com.mysql.jdbc.Driver"
+
+  Class.forName(driver)
+
+  val connection: Connection = DriverManager.getConnection(url,username,password)
+  val statement: Statement = connection.createStatement()
+
+
+}
