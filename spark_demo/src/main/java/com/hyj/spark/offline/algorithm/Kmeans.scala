36b3a9ead78017dd3d42e246191396466e0a5d1e
mozhumz <554501252@qq.com>
2020-07-14 15:48:49 +0800

    Kmeans学习

--- spark_demo/src/main/java/com/hyj/spark/offline/algorithm/Kmeans.scala
+++ spark_demo/src/main/java/com/hyj/spark/offline/algorithm/Kmeans.scala
@@ -2,14 +2,18 @@ package com.hyj.spark.offline.algorithm
 
 import com.huaban.analysis.jieba.JiebaSegmenter.SegMode
 import com.huaban.analysis.jieba.{JiebaSegmenter, SegToken}
+import com.hyj.spark.offline.SparkRDDHyj
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkConf
 import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.ml.evaluation.ClusteringEvaluator
 import org.apache.spark.ml.feature.{HashingTF, IDF}
 import org.apache.spark.sql.functions.{col, udf}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object Kmeans {
   def main(args: Array[String]): Unit = {
+    Logger.getLogger(Kmeans.getClass).setLevel(Level.INFO)
     //    定义结巴分词类的序列化
     val conf = new SparkConf()
       .registerKryoClasses(Array(classOf[JiebaSegmenter]))
@@ -18,7 +22,7 @@ object Kmeans {
     val spark = SparkSession
       .builder()
       .appName("Jieba UDF")
-      .master("local[2]")
+      .master("local[*]")
       .enableHiveSupport()
       .config(conf)
       .getOrCreate()
@@ -37,7 +41,7 @@ object Kmeans {
       df.withColumn("seg",jieba_udf(col(colname)))
     }
 
-    val df = spark.sql("select sentence from badou.news_seg limit 300")
+    val df = spark.sql("select sentence from badou.news_seg ")
 
 //    val df_seg = df.selectExpr("split(sentence,' ') as seg")
 //    调用udf进行结巴切词
@@ -54,7 +58,7 @@ object Kmeans {
     val idf = new IDF()
       .setInputCol("rawFeatures")
       .setOutputCol("features")
-      .setMinDocFreq(2)
+      .setMinDocFreq(1)
 
     val idfModel = idf.fit(df_tf)
     val df_tfidf = idfModel.transform(df_tf).select("features")
@@ -64,19 +68,24 @@ object Kmeans {
       .setFeaturesCol("features")
       .setInitMode("k-means||")
       .setInitSteps(5)
-      .setK(10)
-      .setMaxIter(20)
+      .setK(5)
+      .setMaxIter(30)
       .setPredictionCol("pred")
       .setSeed(2020)
 
     val model = kmeans.fit(df_tfidf)
     val WCSS = model.computeCost(df_tfidf)
+    val loss=model.summary.trainingCost
+    println("loss:"+loss)
     println(s"Set Sum of Squared Errors= $WCSS")
+//    5.797699325240116E7
 //    打印中心点
 
-    println("cluster Centers:")
-    model.clusterCenters.length
+    println("cluster Centers:"+model.clusterCenters.length)
+
+
     model.clusterCenters.map(v=>v.toSparse).foreach(println)
+
 //    为什么中心点会有这么多值
 //    [0,0,0,1] [1,0,0,0] [0,2,0,0] [0,0,4,0]=> [0.25,0.5,1,0.25]
   }
