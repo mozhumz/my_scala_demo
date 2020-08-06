21228feb4cc4c0ddf73118576221eb56acef21d5
mozhumz <554501252@qq.com>
2020-08-06 16:39:04 +0800

    add:lengthOfLongestSubstring

--- algorithm_demo/src/main/java/com/hyj/algorithm/demo/CommonAlgorithm.java
+++ algorithm_demo/src/main/java/com/hyj/algorithm/demo/CommonAlgorithm.java
@@ -2,6 +2,7 @@ package com.hyj.algorithm.demo;
 
 import org.junit.Test;
 
+import java.beans.beancontext.BeanContext;
 import java.util.*;
 
 public class CommonAlgorithm {
@@ -262,5 +263,77 @@ public class CommonAlgorithm {
             val = x;
         }
     }
+
+
+    /**
+     * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+     * 示例 1:
+     * 输入: "abcabcbb"
+     * 输出: 3
+     * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+     * 示例 2:
+     * 输入: "bbbbb"
+     * 输出: 1
+     * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+     * 示例 3:
+     * 输入: "pwwkew"
+     * 输出: 3
+     * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+     */
+    @Test
+    public void lengthOfLongestSubstring(){
+        lengthOfLongestSubstring2("abcabcdde");
+        String str="abcaqwert";
+        Map<Character,Integer>maxMap=new LinkedHashMap<>();
+        maxMap = lengthOfLongestSubstring3(str, maxMap);
+        System.out.println(maxMap);
+    }
+
+    private Map<Character, Integer> lengthOfLongestSubstring3(String str, Map<Character, Integer> maxMap) {
+        Map<Character,Integer>map=new LinkedHashMap<>();
+        int maxLen=0;
+        for(int i = 0; i < str.length(); i ++){
+            for(int j = i; j < str.length(); j ++){
+                if(map.containsKey(str.charAt(j))){
+                    break;
+                }
+                map.put(str.charAt(j),j);
+            }
+            if(map.size()>maxLen){
+                maxLen=map.size();
+                maxMap= map;
+            }
+            map=new HashMap<>();
+        }
+        return maxMap;
+    }
+
+
+    public int lengthOfLongestSubstring2(String s) {
+        int n = s.length(), ans = 0;
+        //最长字符串-起始下标
+        int s1=0;
+        //最长字符串-结尾下标
+        int s2=0;
+        Map<Character, Integer> map = new HashMap<>();
+        int start = 0;
+        for (int end = 0; end < n; end++) {
+            char alpha = s.charAt(end);
+            if (map.containsKey(alpha)) {
+                start = Math.max(map.get(alpha)+1, start);
+            }
+            if(end-start+1>ans){
+                ans=end-start+1;
+                s1=start;
+                s2=end;
+            }
+//            ans = Math.max(ans, end - start + 1);
+            map.put(s.charAt(end), end );
+        }
+        return ans;
+    }
+
+
 
 }
