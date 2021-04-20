c2400ebf39a46950ca74cf85b52be1a74ba5b007
mozhumz <554501252@qq.com>
2021-04-20 21:59:31 +0800

    hive练习

--- spark_demo/src/main/java/com/hyj/hive/createTable.sql
+++ spark_demo/src/main/java/com/hyj/hive/createTable.sql
@@ -8,6 +8,7 @@ order_hour_of_day string,
 days_since_prior_order string
 )
 row format delimited fields terminated by ',';
+--如果从本地导入后本地的原数据文件依然存在，相当于复制过去；如果是从hdfs导入，则原数据文件不存在，相当于剪切过去
 --1 加载本地文件到hive表
 load data local inpath
 '/home/badou/Documents/data/order_data/orders.csv'
