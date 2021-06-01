c50fa85d5c594dc651b2dcf2c3c52e1b585b30f1
mozhumz <554501252@qq.com>
2021-06-01 21:47:46 +0800

    深度优先搜索 链表

--- algorithm_demo/src/main/java/com/hyj/algorithm/demo/LinkedListCase.java
+++ algorithm_demo/src/main/java/com/hyj/algorithm/demo/LinkedListCase.java
@@ -80,4 +80,47 @@ public class LinkedListCase {
         return dummyNode.next;
     }
 
+    /**
+     * 剑指 Offer 18. 删除链表的节点
+     * 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+     * 返回删除后的链表的头节点
+     * 示例 1:
+     *
+     * 输入: head = [4,5,1,9], val = 5
+     * 输出: [4,1,9]
+     * 解释: 给定你链表中值为5的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+     *
+     * 示例 2:
+     * 输入: head = [4,5,1,9], val = 1
+     * 输出: [4,5,9]
+     * 解释: 给定你链表中值为1的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+     *
+     * 说明：
+     * 题目保证链表中节点的值互不相同
+     */
+    @Test
+    public void test20210601(){
+
+    }
+
+    public ListNode deleteNode(ListNode head, int val) {
+        if(head==null){
+            return null;
+        }
+        if(head.val==val){
+            return head.next;
+        }
+        ListNode cur=head;
+        while (cur.next!=null){
+            if(cur.next.val==val){
+                cur.next=cur.next.next;
+                return head;
+            }
+            cur=cur.next;
+        }
+
+        return head;
+    }
 }
+
+
