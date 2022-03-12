ca6d60dc6952ade5641721a6273fc9aca82c4463
mozhumz <554501252@qq.com>
2022-03-12 22:26:10 +0800

    层序数组构建二叉树

--- algorithm_demo/src/main/java/com/hyj/algorithm/demo/tree/TreeTest.java
+++ algorithm_demo/src/main/java/com/hyj/algorithm/demo/tree/TreeTest.java
@@ -2,8 +2,7 @@ package com.hyj.algorithm.demo.tree;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class TreeTest {
     class TreeNode {
@@ -70,50 +69,160 @@ public class TreeTest {
 
     @Test
     public void testBuildTree() {
-        Integer[] arr = {1, null, 0, 0, 1};
-        TreeNode treeNode = build(arr,  new TreeNode(arr[0]));
-        System.out.println(treeNode);
+        Integer[] arr = {3, 9, 20, null, null, 15, 7};
+        TreeNode treeNode = new TreeNode(arr[0]);
+        build(arr, treeNode, 1);
+        printLevelTreeNode(treeNode);
     }
-    private int i=1;
 
-    public TreeNode build(Integer[] arr,  TreeNode root) {
-        if(root==null){
-            return null;
+    public void printLevelTreeNode(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        LinkedList<TreeNode> list = new LinkedList();
+        list.add(root);
+        while (!list.isEmpty()) {
+            TreeNode node = list.poll();
+            res.add(node.val);
+            if (node.left != null) {
+                list.add(node.left);
+            }
+            if (node.right != null) {
+                list.add(node.right);
+            }
+        }
+        System.out.println(res);
+
+    }
+
+    @Test
+    public void build() {
+        Integer[] a = {1,2,3,4,5,6,7};
+        int i=1;
+        TreeNode root = new TreeNode(a[0]);  // 根节点
+        TreeNode current = null;
+        Integer value = null;
+
+        //层序创建二叉树
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while(i<a.length) {
+            current = queue.poll();//从链表中移除并获取第一个节点
+            value = a[i++];
+            if(value!=null) {
+                TreeNode left =new TreeNode(value);
+                current.left=(left);//创建当前节点的左孩子
+                queue.offer(left); // 在链表尾部 左孩子入队
+            }
+            value=a[i++];
+            if(value!=null) {
+                TreeNode right =new TreeNode(value);
+                current.right=(right);//创建当前节点的右孩子
+                queue.offer(right);// 在链表尾部 右孩子入队
+            }
+
+        }
+
+        List<List<Integer>> lists = levelOrder2(root);
+        System.out.println(lists);
+
+    }
+
+    public int build(Integer[] arr, TreeNode root, int i) {
+        if (root == null) {
+            return i;
         }
         if (i >= arr.length) {
-            return root;
+            return i;
         }
-        if(arr[i]!=null){
-            root.left=new TreeNode(arr[i]);
+        if (arr[i] != null) {
+            root.left = new TreeNode(arr[i]);
         }
         i++;
         if (i >= arr.length) {
-            return root;
+            return i;
         }
-        if(arr[i]!=null){
-            root.right=new TreeNode(arr[i]);
+        if (arr[i] != null) {
+            root.right = new TreeNode(arr[i]);
         }
         i++;
-        build(arr,root.left);
-        build(arr,root.right);
+        i = build(arr, root.left, i);
+        i = build(arr, root.right, i);
 
 
-        return root;
+        return i;
     }
 
-    public void printTree(TreeNode root,List<Integer> list){
-        if(root!=null){
-            list.add(root.val);
-            if(root.left!=null){
-                list.add(root.left.val);
+
+    @Test
+    public void testLevelOrder() {
+        Integer[] arr = {1, 2, 3, 4, 5, 6, 7};
+        TreeNode treeNode = new TreeNode(arr[0]);
+        build(arr, treeNode, 1);
+        List<List<Integer>> lists = levelOrder(treeNode);
+        System.out.println(lists);
+    }
+
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        if (root != null) {
+            queue.add(root);
+        }
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
             }
-            if(root.right!=null){
-                list.add(root.right.val);
-            }
-            if(root.left!=null)
-            printTree(root.left.left,list);
-            printTree(root.right,list);
+            res.add(level);
         }
 
+        return res;
+    }
+
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        LinkedList<TreeNode> lk = new LinkedList<>();
+        lk.add(root);
+        Map<TreeNode, Integer> levelMap = new HashMap<>();
+        levelMap.put(root, 1);
+        List<Integer> intList = new ArrayList<>();
+        List<TreeNode> tmpList = new ArrayList<>();
+        while (!lk.isEmpty()) {
+            TreeNode node = lk.poll();
+            int level = levelMap.getOrDefault(node, 0);
+            if (tmpList.isEmpty() || levelMap.getOrDefault(tmpList.get(0), 0) == level) {
+                intList.add(node.val);
+                tmpList.add(node);
+            } else {
+                list.add(intList);
+                intList = new ArrayList<>();
+                intList.add(node.val);
+                tmpList = new ArrayList<>();
+                tmpList.add(node);
+            }
+            if (node.left != null) {
+                lk.add(node.left);
+                levelMap.put(node.left, level + 1);
+            }
+            if (node.right != null) {
+                lk.add(node.right);
+                levelMap.put(node.right, level + 1);
+            }
+        }
+        list.add(intList);
+
+        return list;
     }
 }
