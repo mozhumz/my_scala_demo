f1cb78fb3744688355304efb8e41c54a1df0abab
mozhumz <554501252@qq.com>
2022-09-12 10:10:12 +0800

    20220903：基础算法-二叉树的镜像

--- algorithm_demo/src/main/java/com/hyj/algorithm/demo/DynamicPlan.java
+++ algorithm_demo/src/main/java/com/hyj/algorithm/demo/DynamicPlan.java
@@ -402,4 +402,27 @@ public class DynamicPlan {
         System.out.println(minWord1ToWord2("abc", "abcd"));
         System.out.println(minWord1ToWord2Better("abc", "abcd"));
     }
+
+    /**
+     * 剑指 Offer 27. 二叉树的镜像
+     * 请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+     *
+     */
+    @Test
+    public void test(){
+
+    }
+
+    public TreeNode mirrorTree(TreeNode root) {
+        if(root==null){
+            return null;
+        }
+        mirrorTree(root.left);
+        mirrorTree(root.right);
+        TreeNode tmp=root.left;
+        root.left=root.right;
+        root.right=tmp;
+
+        return root;
+    }
 }
